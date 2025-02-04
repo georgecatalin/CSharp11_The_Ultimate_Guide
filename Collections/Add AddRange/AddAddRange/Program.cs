db5b6dada66ad61f28b7da04038a8e8829b1511b
George Calin <george.calin@chorus.ro>
2025-02-04 16:33:13 +0200

    Add basic list operations

---
+++ Collections/Add AddRange/AddAddRange/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddAddRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create the reference variable on the stack and store the address of  the newly created list class object on the heap
+            List<int> myList = new List<int>() { 10, 20, 30 };
+
+
+            Console.WriteLine();
+            Console.WriteLine("The content of the initial list");
+            //display the contents of the list on the screen
+            foreach (int i in myList) Console.WriteLine(i);
+
+            //Add a new element to the list
+            myList.Add(40);
+
+            Console.WriteLine();
+            Console.WriteLine("The content of the list after adding the new element");
+
+            //display the contents of the list on the screen
+            foreach (int i in myList) Console.WriteLine(i);
+
+            //Add multiple elements at once at the list
+            List<int> secondList = new List<int>() { 40, 50, 60, 70 };
+
+            myList.AddRange(secondList);
+
+            Console.WriteLine();
+            Console.WriteLine("The content of the list after adding multiple  elements at once");
+
+            //display the contents of the list on the screen
+            foreach (int i in myList) Console.WriteLine(i);
+        }
+    }
+}
---
+++ Collections/Insert InsertRange/Insert/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Insert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 10, 20, 30 };
+
+            Console.WriteLine();
+            Console.WriteLine("This is the initial list");
+
+            foreach (int i in myList) Console.WriteLine(i);
+
+
+            myList.Insert(1, 325);
+
+            Console.WriteLine();
+            Console.WriteLine("This is the list after insert an element at position 1");
+
+            foreach (int i in myList) Console.WriteLine(i);
+
+
+            List<int> newList = new List<int>() { 50, 55, 60 };
+            myList.InsertRange(2, newList);
+
+            Console.WriteLine();
+            Console.WriteLine("This is the list after insert a range of elements  at position 2 "); // 10 325 50 55 60 20 30
+            foreach (int i in myList) Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+    }
+}
