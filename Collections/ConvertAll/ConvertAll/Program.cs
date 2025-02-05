5bb5a11cbe92dd1073f52193afb8fa9b8bbddc58
George Calin <george.calin@chorus.ro>
2025-02-05 16:36:06 +0200

    add operations with lists

---
+++ Collections/ConvertAll/ConvertAll/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace ConvertAll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("\nThe initial status of the list is ");
+            foreach (int i in myList) Console.WriteLine(i);
+
+
+            List<string> convertedList = myList.ConvertAll<string>(item =>
+            {
+                string word;
+
+                switch (item)
+                {
+                    case 1: word = "one"; break;
+                    case 2: word = "two"; break;
+                    case 3: word = "three"; break;
+                    case 4: word = "four"; break;
+                    case 5: word = "five"; break;
+                    case 6: word = "six"; break;
+                    case 7: word = "seven"; break;
+                    case 8: word = "eight"; break;
+                    case 9: word = "nine"; break;
+                    default: word = ""; break;
+                }
+
+                return word;
+      
+            });
+
+
+            Console.WriteLine("\ndisplay converted list of strings ");
+
+            foreach (var item in convertedList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Collections/Exists Find FindIndex FindLast/ExistsFind/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExistsFind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 90, 85, 45, 60, 98, 85, 21 };
+
+
+            Console.WriteLine("\ndisplay the initial content of the list");
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            bool isFailed = myList.Exists(mark => mark < 50);
+            Console.WriteLine("\n\nDo there exist failed marks => " + isFailed);
+
+            int firstFailed = myList.Find(mark => mark < 50); //If none found, it returns the default for the data type , in this case int has default 0
+            Console.WriteLine("\nFirst Failed Mark "+ firstFailed);
+
+            int firstFailedIndex = myList.FindIndex(mark => mark <50);
+            Console.WriteLine("\nFirst Failed Index " + firstFailedIndex);
+
+
+            int lastFailed = myList.FindLast(mark => mark < 50);
+            Console.WriteLine("\nLast Failed Mark " + lastFailed);
+
+            int lastFailedIndex = myList.FindLastIndex(mark => mark <50);
+            Console.WriteLine("\nLast Failed Index " + lastFailedIndex);
+
+            List<int> failedMarks = myList.FindAll(mark => mark < 50);
+
+            foreach (var mark in failedMarks)
+            {
+                Console.WriteLine(mark);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
