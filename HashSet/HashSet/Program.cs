72775ec74b2efae0f927b2265e10050e7051c780
George Calin <george.calin@chorus.ro>
2025-02-06 16:38:47 +0200

    Add operations for HashTable and HashSet

---
+++ HashSet/HashSet/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a hashset object
+            HashSet<string> hashset1 = new HashSet<string>()
+            {
+                "George", "Sorina", "Mara", "Sandu"
+            };
+
+            //print the hashset
+            foreach (string s in hashset1)
+            {
+                Console.WriteLine(s);
+            }
+
+            //get the number of elements stored in the hashset, values stored must be unique, only one null allowed
+            int number = hashset1.Count;
+            Console.WriteLine("\nThe number of values in the hashset is " +  number);
+
+
+            //add new element to hashset
+            hashset1.Add("Vali");
+
+            Console.WriteLine("\nAfter adding the new value, this is the hashset count " +  hashset1.Count);
+            //print the hashset
+            foreach (string s in hashset1)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            //remove one value from hashset
+            hashset1.Remove("George"); //remove is case sensitive
+            Console.WriteLine("\nAfter removing the  value, this is the hashset count " + hashset1.Count);
+            //print the hashset
+            foreach (string s in hashset1)
+            {
+                Console.WriteLine(s);
+            }
+
+            //remove value based on condition
+            hashset1.RemoveWhere(value => value.Equals("Mara"));
+            Console.WriteLine("\nAfter removing the  value, this is the hashset count " + hashset1.Count);
+            //print the hashset
+            foreach (string s in hashset1)
+            {
+                Console.WriteLine(s);
+            }
+
+            //verify if the hashset contains a certain value
+            bool containsValue = hashset1.Contains("Mara"); //is case sensitive
+            Console.WriteLine("\nThe hashset contains the Mara value =>" + containsValue);
+
+
+            //clear the hashset collection
+            hashset1.Clear();
+            Console.WriteLine("\nAfter clearing , this is the hashset count " + hashset1.Count);
+            //print the hashset
+            foreach (string s in hashset1)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            //demonstrate unionwith(), intersectwith() methods of the hashset class
+            HashSet<string> countries1 = new HashSet<string>()
+            {
+                "Romania", "Ireland", "Iran", "Israel"
+            };
+
+            HashSet<string> countries2 = new HashSet<string>()
+            {
+                "United States", "Russia", "Hungary"
+            };
+
+
+            countries1.UnionWith(countries2);
+            Console.WriteLine("\nDemonstrate UnionWith()");
+            foreach (string s in countries1)  Console.WriteLine(s);
+
+
+            HashSet<string> cars1 = new HashSet<string>()
+            {
+                "Dacia", "Renault", "Volkswagen", "BMW"
+            };
+
+            HashSet<string> cars2 = new HashSet<string>()
+            {
+                "Dacia", "Renault", "Skoda", "Audi"
+            };
+
+
+            cars1.IntersectWith(cars2);
+            Console.WriteLine("\nDemonstrate IntersectWith()");
+            foreach (string item in cars1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+
+
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //create a hashtable
+            Hashtable hashtable = new Hashtable()
+            {
+                {100, "George" },
+                {101, "Mara" },
+                {102, "Sorina" },
+                {"Pavel", "Sorin" }
+            };
+
+            //add element to hashtable
+            hashtable.Add(105, "Cristian");
+
+
+            //print the hashtable 
+            Console.WriteLine("Printing the hashtable");
+
+            foreach (var item in hashtable)
+            {
+                Console.WriteLine(item);
+            }
+
+            //remove element from hashtable
+            hashtable.Remove(105);
+
+            Console.WriteLine("\nPrinting the hashtable after deleting the element");
+
+            foreach(var item in hashtable)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //get element at hashtable index
+            if (hashtable[100] is string)
+            {
+                Console.WriteLine("Element is " + hashtable[100]);
+                Console.WriteLine();
+            }
+
+            if (hashtable[100] is double)
+            {
+                Console.WriteLine(Convert.ToDouble(hashtable[100]));
+            }
+
+
+            //verify if contains key
+            bool hasKey = hashtable.ContainsKey(100);
+            Console.WriteLine("The hashtable contains the key => " + hasKey);
+
+            //verify if contains value
+            bool hasValue = hashtable.ContainsValue("George");
+            Console.WriteLine("The hashtable contains the value => " +  hasValue);
+
+            //print the keys
+            ICollection keys = hashtable.Keys;
+            Console.WriteLine("\nPrinting the keys in the hashtable");
+            foreach (var key in keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            //print the values
+            ICollection  values = hashtable.Values;
+            Console.WriteLine("\nPrinting the values in the hashtable");
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
