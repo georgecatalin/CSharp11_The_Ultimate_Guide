440bf3cd4f8c61a04f21eaa025a68e75194f4433
George Calin <george.calin@chorus.ro>
2025-02-10 14:45:16 +0200

    add example of using an iterator method and yield return

---
+++ Iterators_yield/Iterators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using IteratorsClassLibrary;
+
+namespace Iterators
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Sample sample = new Sample();
+
+            IEnumerable<int> ienumerable = sample.IteratorMethod();
+            IEnumerator<int> ienumerator = ienumerable.GetEnumerator();
+
+            ienumerator.MoveNext();
+            Console.WriteLine(ienumerator.Current);
+
+            Console.WriteLine("Do something else at this point....");
+
+            ienumerator.MoveNext();
+            Console.WriteLine(ienumerator.Current);
+
+            Console.WriteLine("Do something else at this point.... again");
+
+            ienumerator.MoveNext();
+            Console.WriteLine(ienumerator.Current);
+
+            Console.WriteLine("Another possibility to achieve this , in a single step");
+            foreach (int i in ienumerable)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Iterators_yield/IteratorsClassLibrary/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsClassLibrary
+{
+    public class Sample
+    {
+        public IEnumerable<int> IteratorMethod()
+        {
+            Console.WriteLine("Started the execution of the iterator method");
+            yield return 2011;
+            Console.WriteLine("Continue the execution of the iterator method");
+            yield return 1978;
+            Console.WriteLine("Execute once more the iterator method");
+            yield return 1953;
+        }
+    }
+}
