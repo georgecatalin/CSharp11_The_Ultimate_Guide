fa0bf43e31f21459449bdeb850a2d93a261f9ec7
George Calin <george.calin@chorus.ro>
2025-02-13 14:25:43 +0200

    Add Tuple Class

---
+++ Tuples/Tuple Class/TupleClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TupleClassLib;
+
+
+
+namespace TupleClass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Details details = new Details();
+
+            Console.WriteLine(details.GetDetails().Item1); // here is the first property of the tuple
+            Console.WriteLine(details.GetDetails().Item2); // here is the second property of the tuple
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tuples/Tuple Class/TupleClassLib/Details.cs
@@ -0,0 +1,12 @@
+﻿namespace TupleClassLib
+{
+    public class Details
+    {
+        public Tuple<string,int> GetDetails()
+        {
+            Tuple<string,int> result = new Tuple<string,int>("George Calin", 1978);
+
+            return result;
+        }
+    }
+}
