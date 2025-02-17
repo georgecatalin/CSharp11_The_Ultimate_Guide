4d66422d331eaea916b2ee062aee1224041a6363
George Calin <george.calin@chorus.ro>
2025-02-17 12:26:40 +0200

    added operations with strings

---
+++ Strings/HowStringsAreCreated/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HowStringsAreCreated
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Hello, World!";
+            string y = "Hello, World!"; //the CLR observes internally there is the same System.String object in the heap and it does not create a new object. y points to the same object and contains its address
+
+            Console.WriteLine("x=" +x);
+            Console.WriteLine("y=" + y);
+
+            string z = x; // the address of the string "Hello, World!" in the heap is stored also in z
+            Console.WriteLine("y=" + y);
+
+
+            x = "I am hungry"; // a new System.String object is created on the heap memory and x points to this. x stored its address
+            Console.WriteLine("x=" + x);  // I am hungry
+
+            Console.WriteLine("y=" + y); // Hello, World! -> y still stored the address to the object "Hello, World!" in the heap, nothing changed about it
+            Console.WriteLine("z=" + z); // Hello, World! -> z still stored the address to the object "Hello, World!" in the heap, nothing changed about it
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strings/OperationsWithStrings_1/OperationsWithStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace OperationsWithStrings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string myString = "Developer";
+
+            Console.WriteLine(myString);
+
+            string myStringToLower = myString.ToLower();
+            Console.WriteLine("using ToLower(): "+ myStringToLower);
+            Console.WriteLine("the old string : " + myString);  // the original string does not change. RULE strings are immutable
+
+            string myStringToUpper = myString.ToUpper();
+            Console.WriteLine("\nusing ToUpper(): " + myStringToUpper);
+            Console.WriteLine("the old string : " + myString);  // the original string does not change. RULE strings are immutable
+
+
+            // Developer
+            string vel = myString.Substring(2, 3);
+            Console.WriteLine("\nusing Substring(2,3): " + vel);
+            Console.WriteLine("the old string : " + myString);  // the original string does not change. RULE strings are immutable
+
+            string veloper =  myString.Substring(2);
+            Console.WriteLine("\nusing Substring(2): " + veloper);
+            Console.WriteLine("the old string : " + myString);  // the original string does not change. RULE strings are immutable
+
+            Console.ReadKey();
+        }
+    }
+}
