0a2607a1c9754740c59dbdbff2e69b8692b94051
George Calin <george.calin@chorus.ro>
2025-02-18 13:24:38 +0200

    Add examples for formatting and modifying strings

---
+++ Strings/FormattingStrings/FormattingStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace FormattingStrings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name = "George";
+            string role = "Project Manager";
+
+            string newString = string.Format("{0} has the role of {1}.", name, role);
+
+            Console.WriteLine("\nThe new string obtained with string.Format() " +  newString);
+
+
+            string newStringPlaceholders = $"{name} has the {role}.";
+            Console.WriteLine("\nThe new string obtained with $\" ... \" " + newStringPlaceholders);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Strings/ModifyingStrings/ModifyingStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ModifyingStrings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string myString = "Developer";
+
+            string newStringInsert = myString.Insert(1, "xyx"); //Dxyzeveloper
+
+            Console.WriteLine(newStringInsert);
+
+            string newStringRemove = myString.Remove(2, 3); //      Deoper
+
+            Console.WriteLine(newStringRemove);
+
+            Console.ReadKey();
+
+        }
+    }
+}
