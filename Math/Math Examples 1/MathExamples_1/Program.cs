9b3015ffd76af8c9e6d4a0c3915650faffeaa620
George Calin <george.calin@gmail.com>
2025-02-19 23:19:45 +0200

    Added examples with System.Math static class

---
+++ Math/Math Examples 1/MathExamples_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MathExamples
+{
+    public class Program
+    {
+        public static void Main() 
+        {
+            double power = Math.Pow(10, 3);
+            Console.WriteLine("Math.Power(10, 3)-> " + power);
+
+            double min = Math.Min(20.34, 12.32);
+            Console.WriteLine("Math.Min(20.34, 12.32) ->" + min);
+
+            double max = Math.Max(20.34, 12.32);
+            Console.WriteLine("Math.Max(20.34, 12.32) ->" + max);
+
+            double floor = Math.Floor(23.3522); //23
+            Console.WriteLine("Math.Floor(23.3522) ->" + floor);
+
+            double ceiling = Math.Ceiling(23.3522); //24
+            Console.WriteLine("Math.Ceiling(23.3522) ->" + ceiling);
+
+            double rounddown = Math.Round(10.23); //10
+            Console.WriteLine("Math.Round(10.23) ->" + rounddown);
+
+            double roundup = Math.Round(10.56); //11
+            Console.WriteLine("Math.Round(10.56) -> " + roundup);
+
+            double rounddecimals = Math.Round(10.534567,3); //10.535
+            Console.WriteLine("Math.Round(10.534567) -> " + rounddecimals);
+
+            Console.ReadKey();
+        }
+    }
+}
