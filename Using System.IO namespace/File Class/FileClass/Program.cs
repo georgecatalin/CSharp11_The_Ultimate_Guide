658c42a454c20de8a468b014f3d7eb94d13ac298
George Calin <george.calin@chorus.ro>
2025-02-21 12:05:18 +0200

    Added examples of using File Class to handle files

---
+++ Using System.IO namespace/File Class/FileClass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileClass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //create a file, this recreates a new file in case it already exists
+            string filePath1 = "D:\\Training\\CSharp11_The_Ultimate_Guide\\Using System.IO namespace\\Practice_directory\\New_file.txt"; // or \\ can be escaped with @ in front of "
+            string filePath1_escape_char = @"D:\Training\CSharp11_The_Ultimate_Guide\Using System.IO namespace\Practice_directory\New_file.txt";
+
+            string filePath1_copy = "D:\\Training\\CSharp11_The_Ultimate_Guide\\Using System.IO namespace\\Practice_directory\\New_file_copy.txt";
+            string filePath1_move = "D:\\Training\\CSharp11_The_Ultimate_Guide\\Using System.IO namespace\\Practice_directory\\New_file_move.txt";
+
+            File.Create(filePath1).Close();
+            Console.WriteLine($"{filePath1} was created.");
+
+            //check if the file was created properly
+            bool isCreate = File.Exists(filePath1);
+
+            if (isCreate)
+            {
+                Console.WriteLine($"\nFile {filePath1} was  found.");
+
+                //Copy the file to another file
+                File.Copy(filePath1 , filePath1_copy);
+                Console.WriteLine($"{filePath1} was copied to {filePath1_copy}");
+
+                //Move the file to another file -> File.Move() can be used for renaming the file or moving the file on a location on the same drive letter only
+                File.Move(filePath1 , filePath1_move);
+                Console.WriteLine($"{filePath1_copy} was moved to {filePath1_move}");
+
+                //Delete the file
+                File.Delete(filePath1_move);
+                Console.WriteLine($"{filePath1_move} was deleted.");
+            }
+            else
+            {
+                Console.WriteLine($"\nFile {filePath1} was not found");
+            }
+        }
+    }
+}
---
+++ Using System.IO namespace/Read and Write with File Class/FileClassReadWrite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace FileClassReadWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\Training\CSharp11_The_Ultimate_Guide\Using System.IO namespace\Practice_directory\car_brands.txt";
+            string content = "There are multiple car brands in Europe.";
+
+            string filePath1 = @"D:\Training\CSharp11_The_Ultimate_Guide\Using System.IO namespace\Practice_directory\car_brands.txt";
+
+            //write content to file -> it recreates existing file , if any, at all times when appealed
+            File.WriteAllText(filePath, content);
+            Console.WriteLine($"\n{filePath} \t was created the the following content \t {content} was added to it.");
+
+            //read content from the file
+            string readContent = File.ReadAllText(filePath);
+            Console.WriteLine($"\nI read the following content {readContent} from the file {filePath}");
+
+            //read from List collection and write to file
+            List<string> cars = new List<string>() { "Dacia", "Ford", "Audi" };
+            File.WriteAllLines(filePath1, cars);
+            Console.WriteLine("Lines added to file");
+
+            //read from file into List of strings
+            string[] myArray =File.ReadAllLines(filePath1);
+
+            myArray.ToList().ForEach(x => Console.WriteLine(x));    
+
+            Console.ReadKey();
+        }
+    }
+}
