2f8556b3c2a4bee57c6719e416aad2d7d0a2bac6
George Calin <george.calin@chorus.ro>
2025-02-28 16:26:40 +0200

    added examples using the StreamWriter() and StreamReader() Class

---
+++ Using System.IO namespace/Streamwriter and Streamreader Class/StreamReaderClass/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+
+namespace StreamReaderClass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //there are three ways of creating an object of the StreamReader() class
+            StreamReader streamReader;
+
+            string filePath = @"D:\Training\CSharp11_The_Ultimate_Guide\Using System.IO namespace\practice_here\myfile.txt";
+            string filePath2 = @"D:\Training\CSharp11_The_Ultimate_Guide\Using System.IO namespace\practice_here\readme.txt";
+            //option 1
+            /*
+            streamReader = new StreamReader(filePath);
+            */
+
+            //option 2
+            /*
+            FileInfo fileInfo = new FileInfo(filePath);
+            streamReader = fileInfo.OpenText();
+            */
+
+            //option 2
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            using (streamReader = new StreamReader(fs))
+            {
+                string all_content = streamReader.ReadToEnd();
+                Console.WriteLine(all_content);
+            }
+
+
+            Console.WriteLine("\n\nThis is the content of the file read all at once");
+
+
+            using (StreamReader sr1  = new StreamReader(filePath))
+            {
+
+                int char_read;
+
+                do
+                {
+                    char[] char_array = new char[50];
+                    char_read = sr1.Read(char_array, 0, char_array.Length);
+
+                    string what_has_been_read = new string(char_array);
+                    Console.WriteLine(what_has_been_read);
+                }while (char_read > 0);
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Using System.IO namespace/Streamwriter and Streamreader Class/StreamWriterClass/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+
+namespace StreamWriterClass
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // StreamWriter() class can be used for writing to text without the need of converting the string to byte arrays.But can only be used with text files, no binary files
+
+
+            string filePath = @"D:\Training\CSharp11_The_Ultimate_Guide\Using System.IO namespace\practice_here\readme.txt";
+            string filePath2 = @"D:\Training\CSharp11_The_Ultimate_Guide\Using System.IO namespace\practice_here\readme2.txt";
+            StreamWriter streamWriter;
+
+            //there exist multiple ways to create a streamwriter object
+            /*
+            streamWriter = new StreamWriter(filePath);
+            */
+
+            //the object can be created with the constructor using the Filestream
+            /*
+            FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            streamWriter = new StreamWriter(fileStream);
+            */
+
+            //the object can be created using methods of FileInfo() class
+            FileInfo fileInfo = new FileInfo(filePath);
+            streamWriter = fileInfo.CreateText(); // OR fileInfo.AppendText()
+
+            streamWriter.WriteLine("This is the first line.");
+            streamWriter.WriteLine("This is the second line.");
+            streamWriter.WriteLine("This is the thirs line.");
+
+            streamWriter.Close();
+
+            //another of closing the open resources is using
+
+            using (streamWriter = new StreamWriter(filePath2))
+            {
+                streamWriter.WriteLine("This line is written in the using statement");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
