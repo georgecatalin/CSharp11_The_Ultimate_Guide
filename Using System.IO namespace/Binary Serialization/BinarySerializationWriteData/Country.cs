332bb4d19d6f5045c9efef894030765ff7898deb
georgeca <george.calin@gmail.com>
2025-03-01 19:20:42 +0200

    Added example for binary serialization of the object state to a file

---
+++ Using System.IO namespace/Binary Serialization/BinarySerializationWriteData/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerializationWriteData
+{
+    [Serializable]
+    class Country
+    {
+        public short countryId { get; set; }
+        public required string countryName { get; set; }
+        public required string countryRegion { get; set; }
+    }
+}
