deff0559cdd5177088747450bcf89c6c1591a652
georgeca <george.calin@gmail.com>
2025-03-01 20:47:35 +0200

    Added example for json serialization

---
+++ Using System.IO namespace/JSON Serialization/JSONSerialization/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONSerialization
+{
+    [Serializable] // Correct the attribute name
+    class Customer
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public short Age { get; set; }
+    }
+}
