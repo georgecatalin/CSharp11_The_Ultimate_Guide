fbd07b781306d1a414d635de81ed4791e185715a
George Calin <george.calin@chorus.ro>
2025-03-03 12:49:54 +0200

    Add example for try-catch-finally

---
+++ Exception Handling/TryCatchFinally/TryCatchFinally/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+namespace TryCatchFinally
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int a, b;
+                Console.WriteLine("Enter the first number :");
+                string first = Console.ReadLine();
+
+                a = int.Parse(first);
+
+                Console.WriteLine("Enter the second number:");
+                string second = Console.ReadLine();
+
+                b = int.Parse(second);
+
+                int c = a / b;
+                Console.WriteLine("The division result is " + c);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
