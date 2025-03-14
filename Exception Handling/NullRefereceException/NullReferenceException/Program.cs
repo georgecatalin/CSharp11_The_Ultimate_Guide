a23e640f5c5b282685f178981d156626cdca1336
George Calin <george.calin@chorus.ro>
2025-03-14 12:22:30 +0200

    NullReferenceException example

---
+++ Exception Handling/NullRefereceException/NullReferenceException/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+
+namespace NullReferenceExceptionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account georgeAccount = new Account() { AccountId = 100, AccountHolderName = "George", CurrentValue = 10000 };
+            Account maraAccount = new Account() { AccountId = 101, AccountHolderName = "Mara", CurrentValue = 50000 };
+            Account sorinaAccount = null;
+
+            try
+            {
+                TransferFunds transferFunds = new TransferFunds();
+                transferFunds.TransferFund(maraAccount, maraAccount, 500);
+
+                transferFunds.TransferFund(maraAccount, sorinaAccount, 1000); // this will throw NullReferenceException because sorinaAccount is null
+
+                Console.WriteLine("Money transferred successfully.");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("NullReferenceException in the outer method");
+            }
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Exception Handling/NullRefereceException/NullReferenceException/TransferFunds.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullReferenceExceptionExample
+{
+    internal class TransferFunds
+    {
+        public void TransferFund(Account sourceAccount, Account destinationAccount, double amount)
+        {
+            try
+            {
+                sourceAccount.CurrentValue -= amount;
+                destinationAccount.CurrentValue += amount;
+
+                Console.WriteLine($"We transferred from {sourceAccount.AccountHolderName} to {destinationAccount.AccountHolderName} the amount of {amount}");
+            }
+            catch( NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Invalid destination account");
+                throw; // throw the same exception to the calling method to be also visible there
+            }
+        }
+    }
+}
