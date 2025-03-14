699f16ba743a3593cfd76252a5d9ee6a8cd72e27
George Calin <george.calin@chorus.ro>
2025-03-14 16:18:08 +0200

    an example how to print the StackTrace property of the Exception() object

---
+++ Exception Handling/StackTrace/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptionExample
+{
+    internal class Account
+    {
+        public int AccountId { get; set; }  
+        public required string  AccountHolderName { get; set; }
+
+        private double _currentValue;
+        public double CurrentValue 
+        {
+            get => _currentValue;
+
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("This is called in the setter of the property", "CurrentValue"); // this creates an object of the class ArgumentException() which will be caught in the main() method
+                }
+                else
+                {
+                    _currentValue = value;
+                }
+                
+            }
+        }
+    }
+}
