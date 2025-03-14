9c1688daf3aa24dfd9240f5daf965f9a7dfad2b5
George Calin <george.calin@chorus.ro>
2025-03-14 16:06:10 +0200

    How to create a custom exception example

---
+++ Exception Handling/CustomException/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+
+namespace CustomExceptionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                Account georgeAccount = new Account() { AccountId = 100, AccountHolderName = "George", CurrentValue = 1000 };
+                Account maraAccount = new Account() { AccountId = 101, AccountHolderName = "Mara", CurrentValue = 5000 }; //  this will throw ArgumentException() cause the setter of CurrentValue is not validated in the constructor
+                Account sorinaAccount = null;
+
+
+                TransferFunds transferFunds = new TransferFunds();
+                transferFunds.TransferFund(georgeAccount, maraAccount, 2000); //this triggers  an InvalidOperationException() as per the TransferFunds() method
+
+                transferFunds.TransferFund(maraAccount, sorinaAccount, 1000); //
+
+                Console.WriteLine("Money transferred successfully.");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Same here: this is run in the outer method with InnerException -> "+ ex.InnerException.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ActualValue);
+                Console.WriteLine(ex.ParamName);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("EXCEPTION MESSAGE: " + ex.Message);
+                Console.WriteLine("EXCEPTION PARAMETER: " + ex.ParamName);
+            }
+            catch (InsufficientFundsException ex)
+            {
+                Console.WriteLine(ex.Message); // at all times catch the child Exception() before catching the parent Exception()
+            }
+            catch (InvalidOperationException ex) // this catches the object of InvalidOperationException which was raised in the TransferFund() method
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
