4e119e588d744fb243a7d1beaee268745aa901a6
George Calin <george.calin@chorus.ro>
2025-03-14 15:19:21 +0200

    ArgumentException() example

---
+++ Exception Handling/ArgumentException/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+
+namespace NullReferenceExceptionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                Account georgeAccount = new Account() { AccountId = 100, AccountHolderName = "George", CurrentValue = 10000 };
+                Account maraAccount = new Account() { AccountId = 101, AccountHolderName = "Mara", CurrentValue = -5000 }; //  this will throw ArgumentException() cause the setter of CurrentValue is not validated in the constructor
+                Account sorinaAccount = null;
+
+
+                TransferFunds transferFunds = new TransferFunds();
+                transferFunds.TransferFund(georgeAccount, maraAccount, 500); 
+
+                transferFunds.TransferFund(maraAccount, sorinaAccount, -1000); //it does not get to this as the exception is caught in the catch()
+
+                Console.WriteLine("Money transferred successfully.");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Same here: this is run in the outer method with InnerException -> "+ ex.InnerException.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ActualValue);
+                Console.WriteLine(ex.ParamName);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("EXCEPTION MESSAGE: " + ex.Message);
+                Console.WriteLine("EXCEPTION PARAMETER: " + ex.ParamName);
+            }
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Exception Handling/ArgumentException/TransferFunds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullReferenceExceptionExample
+{
+    internal class TransferFunds
+    {
+        public void TransferFund(Account sourceAccount, Account destinationAccount, double amount)
+        {
+            try
+            {
+                if(amount > 5000)
+                {
+                    throw new ArgumentOutOfRangeException("amount", 5000, "This is called from the inner method: argument is out of range");
+                }
+                sourceAccount.CurrentValue -= amount; //in case of exception this creates an object of that exception
+            }
+            catch( NullReferenceException ex)
+            {
+                Console.WriteLine("This is from inner method: " + ex.Message);
+                throw new ArgumentNullException( "This is from inner method: There is a null parameter in the source", ex); // throw the same exception to the calling method to be also visible there
+            }
+
+
+            try
+            {
+                destinationAccount.CurrentValue += amount;
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("This is from inner method: " + ex.Message);
+                throw new ArgumentNullException("This is from outer method: There is a null parameter in the destination", ex); // throw the same exception to the calling method to be also visible there
+            }
+        }
+    }
+}
