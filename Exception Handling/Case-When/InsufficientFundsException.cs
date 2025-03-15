5616250cae9d69be6bb21bd55534a4286989193d
George Calin <george.calin@gmail.com>
2025-03-15 07:10:09 +0200

    added an example using case-when

---
+++ Exception Handling/Case-When/InsufficientFundsException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseWhen
+{
+    internal class InsufficientFundsException : InvalidOperationException
+    {
+
+        public InsufficientFundsException() 
+        { 
+        
+        }
+
+        public InsufficientFundsException(string message) : base(message)
+        {
+            Console.WriteLine("This is called from the child custom exception");
+        }
+
+        public InsufficientFundsException(string message, Exception ex) : base(message, ex)
+        {
+
+        }
+
+    }
+}
