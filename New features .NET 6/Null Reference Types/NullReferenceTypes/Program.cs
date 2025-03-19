4b39e69eb63e9e4d8d357fdbec0c5e95b9979598
George Calin <george.calin@chorus.ro>
2025-03-19 12:35:33 +0200

    Added examples for nullable types

---
+++ New features .NET 6/Null Reference Types/NullReferenceTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿// class nameOfVariable -> this is a non-nullable type
+// class? nameOfVariable -> this is a nullable type
+/* C# 9 allows the possibility of a static reference type analysis providing warnings in early time to avoid null-reference errors at runtime */
+
+
+namespace NullReferenceTypes
+{
+    public class Employee
+    {
+        public int x { get; set; }
+    }
+
+    public class  EmployeeBusinessLogic
+    {
+        public Employee? GetEmployee()
+        {
+            return null; ;
+        }
+    }
+        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { x = 125 };
+            Console.WriteLine(employee.x);
+
+
+            EmployeeBusinessLogic  employeeBusinessLogic = new EmployeeBusinessLogic();
+            Employee? employee1 =  employeeBusinessLogic.GetEmployee();
+
+            if (employee1 == null)
+            {
+                Console.WriteLine("There is a null reference here");
+            }
+            else
+            {
+                Console.WriteLine(employee1.x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
