5c49fa59e2faef006447d6f818949ce0088d8a19
George Calin <george.calin@chorus.ro>
2025-03-19 10:35:16 +0200

    add example for file-scoped-namespaces

---
+++ New features .NET 6/File Scoped Namespaces/FileScopedNamespaces/FileScopedNamespaces/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace file_scoped_namespace;
+
+public class Sample1
+{
+
+}
+
+public class Sample2
+{
+
+}
---
+++ New features .NET 6/File Scoped Namespaces/FileScopedNamespaces/FileScopedNamespaces/otherfile.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello from class");
+
+
+public class Sample3
+{
+    file_scoped_namespace.Sample1  thisobject = new file_scoped_namespace.Sample1();
+}
