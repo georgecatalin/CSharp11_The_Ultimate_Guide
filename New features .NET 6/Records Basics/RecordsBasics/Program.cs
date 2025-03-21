215063feaa07147710d7d523a8a39230ff784f5d
George Calin <george.calin@chorus.ro>
2025-03-21 16:52:22 +0200

    Add a basic example of using the record reference type which holds immutable properties with only get, init accessors.

---
+++ New features .NET 6/Records Basics/RecordsBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Records are shortened manners to create reference-types that hold immutable properties */
+
+namespace RecordsBasics
+{
+    public record Person(string Name, int Age);
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from RECORDS BASICS");
+
+
+            Person person1 = new Person("George Calin", 46);
+            Person person2 = new("Mara Calin", 14); // how to appeal the constructor of the record using the new() expression
+
+            //the immutable properties of the record Person can only by initialized through the constructor, but these can not be modified at a later time
+            Console.WriteLine("{0}-{1}",person1.Name,person1.Age);
+            Console.WriteLine("{0}-{1}", person2.Name, person2.Age);
+
+            //but once the property is initialized , it can not longer be changed
+            //person1.Age = 47;
+
+            Console.ReadKey();
+        }
+    }
+}
