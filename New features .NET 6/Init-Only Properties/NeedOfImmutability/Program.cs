8c2d1b3cc2adc26237a461c7384cbf96b66b52e7
George Calin <george.calin@chorus.ro>
2025-03-21 15:08:17 +0200

    Added example with init-only properties

---
+++ New features .NET 6/Init-Only Properties/NeedOfImmutability/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace NeedOfImmutability
+{
+    public class BankAccount
+    {
+        private readonly int _accountNumber;
+        private readonly double _currentBalance;
+
+        //the readonly(immutable) fields can only be instantiated in constructors(parameterized or not), but not in normal methods
+        public BankAccount()
+        {
+            
+        }
+
+        //we shall use readonly properties to access the values from outside the class
+        public int AccountNumber { get => _accountNumber; init => _accountNumber = value; } // these init-only classes can only be initialized inline, in constructors or in objects initializers
+        public double CurrentBalance { 
+            get => _currentBalance;
+            init
+            {
+                if(value > 0)
+                {
+                    _currentBalance = value;
+                }
+            }
+        }
+    }
+
+    public class DataStorage
+    {
+        public static List<BankAccount> GetBankAccounts()
+        {
+            return new List<BankAccount>
+            {
+                new BankAccount(){AccountNumber = 100, CurrentBalance=123.5 },
+                new BankAccount(){AccountNumber = 125, CurrentBalance= 435.6 }
+            };
+        }
+
+        public static double GetCurrentBalance(BankAccount bankAccount)
+        {
+            //bankAccount.CurrentBalance = 234.5; // by mistake the property can be modified outside the class -> here comes the need for immutability
+            return bankAccount.CurrentBalance;
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                List<BankAccount> bankAccounts = GetBankAccounts();
+                BankAccount firstBankAccount = bankAccounts[0];
+                double balance = firstBankAccount.CurrentBalance;
+
+                Console.WriteLine("The balance is {0} ", balance);
+
+            }
+        }
+    }
+}  
