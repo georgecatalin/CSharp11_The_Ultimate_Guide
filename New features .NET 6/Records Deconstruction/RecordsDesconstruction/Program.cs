fb0c784dae51fe40db8cede4ed0770415660402b
George Calin <george.calin@chorus.ro>
2025-03-25 15:08:47 +0200

    Added examples for deconstructing records and using ToString() method

---
+++ New features .NET 6/Records Deconstruction/RecordsDesconstruction/Program.cs
@@ -0,0 +1,30 @@
+﻿/* C# provides out of the box a method for the deconstruction of records into a tuple of its properties */
+
+
+using System.Net.Sockets;
+
+namespace RecordsDesconstruction
+{
+    public record Person(string Name, int Age, Address personAddress);
+ 
+    public record Address(string City, string Country);
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("George Calin", 46, new Address("Galati", "Romania"));
+            var (theName, theAge, theAddress) = person;
+
+            Console.WriteLine("\nThe deconstruction of a record");
+            Console.WriteLine($"{theName} {theAge} {theAddress.City} {theAddress.Country} ");
+
+            //Deep deconstruction of the nested record
+            var (theName1,theAge1,(theCity,theCountry)) = person;
+            Console.WriteLine($"\n,{theName1},{theAge1}, {theCity} {theCountry}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ New features .NET 6/Records ToString/RecordsToStringMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿/* C# provides the ToString() method for records, and this can be overridden at will */
+
+namespace RecordsToStringMethod
+{
+    public record Address(string City, string Country)
+    {
+        //supposedly we wish to override the ToString() method to show stuffs in a certain manner
+        public override string ToString()
+        {
+            return $"The address is {City} in {Country}";
+            
+        }
+    }
+    public record Person(string Name, int Age, Address personAddress);
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("George Calin", 46, new Address("Galati", "Romania"));
+            Console.WriteLine(person.ToString()); // ToString() returns a string with the record and the properties and their values
+            /* Person { Name = George Calin, Age = 46, personAddress = Address { City = Galati, Country = Romania } } if the ToString() is not overriden */
+
+            Console.ReadKey();
+
+
+      
+        }
+    }
+}
