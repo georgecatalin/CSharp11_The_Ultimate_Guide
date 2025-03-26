4e849c14400ba4998ca8574527f50ee32b758a4c
George Calin <george.calin@chorus.ro>
2025-03-26 16:36:37 +0200

    Added examples how to use private non-abstract(default) methods in interfaces and static methods in later versions of C#

---
+++ New features .NET 6/Interface Private Methods/InterfacePrivateMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿/* It is possible to use private interface methods, but only with default methods (that have a body implementation)
+ * it makes no sense to use private abstract interface methods, as these could not ne inherited and herein implemented in the child classes */
+
+namespace InterfacePrivateMethods
+{
+    public interface IEmployee
+    {
+        string Name { get; set; }
+        private string GetGreeting()
+        {
+            return "Mr/Mrs";
+        }
+
+        //the private non-abstract methods can be used in other non-abstract methods of the interface
+        public string GetName()
+        {
+            return GetGreeting() + " " + Name;
+        }
+
+    }
+
+    public class Employee : IEmployee
+    {
+        public string Name { get; set; }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Test";
+
+            IEmployee iemployee = (IEmployee) employee;
+            Console.WriteLine(iemployee.GetName());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ New features .NET 6/Interface Static Members/InterfacePrivateMethods/Program.cs
@@ -0,0 +1,45 @@
+﻿/* It is possible to use private interface methods, but only with default methods (that have a body implementation)
+ * it makes no sense to use private abstract interface methods, as these could not ne inherited and herein implemented in the child classes */
+
+namespace InterfacePrivateMethods
+{
+    public interface IEmployee
+    {
+        string Name { get; set; }
+        public static string GetGreeting()
+        {
+            return "Mr/Mrs";
+        }
+
+        //the private non-abstract methods can be used in other non-abstract methods of the interface
+        public string GetName()
+        {
+            return IEmployee.GetGreeting() + " " + Name; //use the static interface method from inside the interface
+        }
+
+    }
+
+    public class Employee : IEmployee
+    {
+        public string Name { get; set; }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Name = "Test";
+
+            IEmployee iemployee = (IEmployee) employee;
+            Console.WriteLine(iemployee.GetName());
+            Console.WriteLine("\nuse the static interface method from outside the interface");
+            Console.WriteLine(IEmployee.GetGreeting()); // use the static interface method from outside the interface
+
+            Console.ReadKey();
+        }
+    }
+}
