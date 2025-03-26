a504831b9d8c815f790e62ba09d3fe8ab09f259e
George Calin <george.calin@chorus.ro>
2025-03-26 14:55:32 +0200

    Added an example how to use string interpolation in constants after C#10

---
+++ New features .NET 6/Constant interpolated strings/StringInterpolationInConstants/Program.cs
@@ -0,0 +1,24 @@
+﻿/* From C# 10 onwards it is possible to use string interpolation in constants with text from other constants */
+
+
+namespace StringInterpolationInConstants
+{
+    public class Example
+    {
+        public const string BaseURL = "http://www.example.com";
+        public const string APIURL = $"{BaseURL}/Api"; // constants are replaced at compile time with their textual content whilst any other static members are replaced at runtime
+
+    }
+
+
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Example.APIURL);
+            Console.ReadKey();
+        }
+    }
+}
