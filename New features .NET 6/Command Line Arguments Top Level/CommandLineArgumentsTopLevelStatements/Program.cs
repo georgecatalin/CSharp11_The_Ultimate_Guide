0162803493bcdad7ea69d1ee8303c6e363a47541
George Calin <george.calin@chorus.ro>
2025-03-26 12:48:32 +0200

    Added examples of how to use command line arguments

---
+++ New features .NET 6/Command Line Arguments Top Level/CommandLineArgumentsTopLevelStatements/Program.cs
@@ -0,0 +1,22 @@
+﻿string filePath = "D:\\Training\\CSharp11_The_Ultimate_Guide\\New features .NET 6\\Sandbox\\readme.txt";
+
+Console.WriteLine($"There are {args.Length} arguments read from the keyboard in terminal window");
+
+if(args.Length == 1)
+{
+    if(File.Exists(filePath))
+    {
+        string contentOfFile = File.ReadAllText(filePath);
+        Console.WriteLine(contentOfFile);
+    }
+    else
+    {
+        Console.WriteLine("The file does not exist at the path you provided as argument");
+    }
+}
+else
+{
+    Console.WriteLine("You must provide an adequate number of arguments");
+}
+
+Console.ReadKey();
---
+++ New features .NET 6/Command Line Arguments static main/CommandLineArgumentsStaticMain/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CommandLineArgumentsStaticMain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There are {0} arguments", args.Length);
+
+            int sum = 0;
+            if(args.Length > 0)
+            {
+                foreach (string arg in args)
+                {
+                    if(int.TryParse(arg, out int result))
+                    {
+                        sum += result;
+                    }
+
+                }
+            }
+
+            Console.WriteLine("The sum of the arguments is {0}.", sum);
+            Console.ReadKey();
+
+        }
+    }
+}
