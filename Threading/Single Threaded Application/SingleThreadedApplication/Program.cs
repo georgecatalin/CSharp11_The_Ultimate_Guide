431687d1b371dbd25dfe06f124b7efc6df0db10f
George Calin <george.calin@chorus.ro>
2025-03-27 12:56:05 +0200

    Added an example of a single threaded application, all methods called from main() are executed in the thread of main()

---
+++ Threading/Single Threaded Application/SingleThreadedApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SingleThreadedApplication
+{
+    public class Counter
+    {
+        public void CountUp()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < 100; i++)
+            {
+               
+                Console.WriteLine($"i = {i.ToString()},");
+            }
+        }
+
+        public void CountDown()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            for (int i = 100; i >= 0; i--)
+            {
+                Console.WriteLine($"i = {i.ToString()},");
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from \"SingleThreadedApplication\" ");
+
+
+            Thread thisThread = Thread.CurrentThread;
+            thisThread.Name = "my thread";
+            Console.WriteLine("\nName of the thread is: " + thisThread.Name);
+
+            Counter counter = new Counter();
+            counter.CountUp();
+            counter.CountDown();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("the execution of the thread has been completed => " + thisThread.Name);
+            Console.ReadKey();
+        }
+    }
+}
