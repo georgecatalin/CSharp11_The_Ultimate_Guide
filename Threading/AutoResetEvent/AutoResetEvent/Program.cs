c7783403c60e620edf3dee08c822e69ed41702dc
George Calin <george.calin@chorus.ro>
2025-03-31 16:41:14 +0300

    more examples for thread signaling

---
+++ Threading/AutoResetEvent/AutoResetEvent/Program.cs
@@ -0,0 +1,106 @@
+﻿/* ManualResetEvent verus AutoResetEvent
+ * ManualResetEvent -> When signalling using the Set() method, it stays signaled until the Reset() method is called to make it unsignaled. If there are multiple threads to read the same data in parallel, then manualResetEvent is better
+ * AutoResetEvent ->  Wheen signalling using the Set() method, it gets automatically unsignaled after releasing one waiting thread. If only one thread wishes to read, then AutoResetEvent is better
+ */
+
+
+namespace ManualResetEventExample
+{
+    public class Shared
+    {
+        public static int[] Data { get; set; }
+        public static int BatchCount { get; set; }
+        public static AutoResetEvent ThisEvent { get; set; }
+        public static int BatchSize { get; set; }
+
+        static Shared()
+        {
+
+            Data = new int[15];
+            BatchCount = 5;
+            BatchSize = 3;
+            ThisEvent = new AutoResetEvent(false);
+        }
+    }
+
+
+    public class Producer
+    {
+        public void Produce()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} started ....");
+
+            //generate the data
+            for (int i = 0; i < Shared.BatchCount; i++)
+            {
+                for (int j = 0; j < Shared.BatchSize; j++)
+                {
+                    Shared.Data[i * Shared.BatchSize + j] = i * Shared.BatchSize + j + 1;
+                    Thread.Sleep(300); //set an artificial delay         
+                }
+                //send the signal to the listening thread that the data was generated and to execute its sequence only once
+                Shared.ThisEvent.Set(); // ThisEvent is set to True
+            }
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} completed.");
+        }
+    }
+
+
+    public class Consumer
+    {
+        public void Consume()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} started ....");
+
+            for (int i = 0; i < Shared.BatchCount; i++)
+            {
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} is waiting for a signal from the Producer Thread");
+                Shared.ThisEvent.WaitOne();
+                Console.WriteLine($"{Thread.CurrentThread.Name} just received the signal that the data is ready...");
+
+                //read the data prepared in the Producer Thread
+                for (int j = 0; j < Shared.BatchSize; j++)
+                {
+
+                    Console.WriteLine(Shared.Data[i * Shared.BatchSize + j]);
+                }
+            }
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} completed.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create objects of the classes
+            Producer producer = new Producer();
+            Consumer consumer = new Consumer();
+
+            //create delegates of the ThreadStart
+            ThreadStart producerThreadStart = new ThreadStart(producer.Produce);
+            ThreadStart consumerThreadStart = new ThreadStart(consumer.Consume);
+
+            //create the thread objects
+            Thread producerThread = new Thread(producerThreadStart) { Name = "Producer Thread" };
+            Thread consumerThread = new Thread(consumerThreadStart) { Name = "Consumer Thread" };
+
+
+            //start the threads
+            producerThread.Start();
+            consumerThread.Start();
+
+            //join the threads so that the main thread waits for their completion before finishing itself
+            producerThread.Join();
+            consumerThread.Join();
+
+
+            Console.WriteLine("The main thread completed");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Threading/Thread Signaling with Monitor Wait and Pulse/MonitorWaitPulse/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace MonitorWaitPulse
+{
+    public class Shared
+    {
+        public static object LockObject = new object(); // the object needed for using the Monitor() class
+        public static Queue<int> Buffer = new Queue<int>(); // the buffer to store the data values
+        public const int BufferCapacity = 5; // the maximum capacity of the Buffer
+
+        public static void Print()
+        {
+            Console.WriteLine("Buffer:");
+
+            foreach (int i in Buffer)
+            {
+                Console.WriteLine($"{i},");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    public class Producer
+    {
+        public void Produce()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} has started ...");
+
+            for (int i = 0; i<10;i++)
+            {
+                lock(Shared.LockObject)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} is generating data...");
+                    Thread.Sleep(5000); //introduce an artificial latency
+
+                    if (Shared.Buffer.Count == Shared.BufferCapacity) //verify if the buffer is full
+                    {
+                        Console.WriteLine("The buffer is full.Waiting for the consumer to consume stuffs...");
+                        Monitor.Wait(Shared.LockObject);
+                    }
+
+                    Shared.Buffer.Enqueue(i); // the buffer has at least one place available , hence add a new element
+                    Console.WriteLine($"{Thread.CurrentThread.Name} produced a new element -> {i} and added it to the buffer");
+                    Shared.Print();
+                    //inform the Consumer that a new element has been added
+                    Monitor.Pulse(Shared.LockObject);
+                } 
+            }
+           
+            Console.WriteLine($"{Thread.CurrentThread.Name} was completed ...");
+        }
+    }
+
+    public class Consumer
+    {
+        public void Consume()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} has started ...");
+
+            for (int i = 0; i< Shared.BufferCapacity; i++)
+            {
+                lock(Shared.LockObject) // it is safe to access a shared resource in a multithread environment only in a locked state for that thread
+                {
+                    if(Shared.Buffer.Count == 0)
+                    {
+                        Console.WriteLine("The buffer is empty. Waiting for a signal from the producer...");
+                        Monitor.Wait(Shared.LockObject);
+                    }
+                }
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} is consuming the data.");
+                Thread.Sleep(2500); //introduce an artificial latency of 2.5 seconds
+
+                //consume the data
+                lock (Shared.LockObject)
+                {
+                    int value = Shared.Buffer.Dequeue();
+                    Console.WriteLine($"{Thread.CurrentThread.Name} consumed \t -> {value}");
+
+                    //inform the Producer that at least one value is available
+                    Monitor.Pulse(Shared.LockObject);
+                }
+
+
+            }
+
+
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} was completed ...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //create object of the classes
+           Producer producer = new Producer();
+           Consumer consumer = new Consumer();
+
+            //create thread objects -> note the threads can be created without creating delegates of the ThreadStart() , but by utilizing an overload of the Thread() constructor
+            Thread producerThread = new Thread(producer.Produce) { Name = "Producer Thread" };
+            Thread consumerThread = new Thread(consumer.Consume) { Name = "Consumer Thread" };
+
+            //start the threads
+            producerThread.Start();
+            consumerThread.Start();
+
+            //join the threads
+            producerThread.Join();
+            consumerThread.Join();
+
+            Console.ReadKey();
+        }
+    }
+}
