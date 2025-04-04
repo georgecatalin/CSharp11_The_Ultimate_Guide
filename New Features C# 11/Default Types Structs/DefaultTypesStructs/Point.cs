683062a9b68b0181d5f1e2d852ea9ec84d607ae2
George Calin <george.calin@chorus.ro>
2025-04-04 16:17:37 +0300

    understand the introduction of default type to constructors of Structs in C# 11

---
+++ New Features C# 11/Default Types Structs/DefaultTypesStructs/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultTypesStructs
+{
+    internal struct Point
+    {
+        public int x;
+        public int y;
+        public string Name;
+
+
+        //you can add the constructor, but anyway C# 11 creates a parameterless constructor by default
+    }
+}
---
+++ New Features C# 11/Default Types Structs/DefaultTypesStructs/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Notes:
+ * C# 11 provides by default a constructor for structs which sets the default values into fields, based on their individual types
+ */
+
+namespace DefaultTypesStructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+
+            Console.WriteLine(point.Name); // null
+            Console.WriteLine(point.x); // 0 is default value for int
+            Console.WriteLine(point.y); // 0 is default value for int
+
+            Console.ReadKey();
+        }
+    }
+}
