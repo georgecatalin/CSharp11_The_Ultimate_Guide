51719a256b25ef956a70b4c57fa06798de326c9c
George Calin <george.calin@chorus.ro>
2025-04-04 16:08:19 +0300

    Understand about required members

---
+++ New Features C# 11/Required members/RequiredMembers/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequiredMembers
+{
+    internal class Person
+    {
+        public required string Name { get; set; }
+        public int Age { get; set; }
+
+        [SetsRequiredMembers]
+        public Person() //parameterless constructor
+        {
+            Name = "Anonymous";
+        }
+
+        [SetsRequiredMembers]
+        public Person(string name) //parameterized constructor
+        { this.Name = name; }
+    }
+}
---
+++ New Features C# 11/Required members/RequiredMembers/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Notes:
+ * 1. Required members are newly introduced in C# 11 to ease against null checking and protect against runtime errors
+ * 2. Required members are properties or fields that have to be initialized either in the parameterless or parameterized constructor or in the object initializer otherwise there is a compiler error
+ * 3. When initializing the required member in the constructor this has to be decorated with a decorator      [SetsRequiredMembers] to let the compiler know that the members is initialized in that constructor
+ * */
+
+
+namespace RequiredMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { Name = "Test" };
+
+            Console.WriteLine(person.Name);
+
+            Person person2 = new Person();
+            Console.WriteLine(person2.Name);
+
+        }
+    }
+}
