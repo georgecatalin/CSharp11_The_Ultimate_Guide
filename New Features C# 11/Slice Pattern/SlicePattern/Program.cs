fceca3dce3459ac8c19dc299253d468bb3944969
George Calin <george.calin@chorus.ro>
2025-04-04 14:09:48 +0300

    explain the newly introduced concept of pattern matching in C# 11

---
+++ New Features C# 11/Slice Pattern/SlicePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿/* NOTES:
+ * 1. the slice pattern can be used within list patterns and has been added in C# 11 (.NET 7.0*)
+ * 2. the slice pattern can be used either at the beginning, in the middle or at the end of the list pattern
+ * */
+
+
+namespace SlicePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new List<int>() { 10, 25, 50 } is [10, < 26, 50 or 150]); // returns true
+
+            Console.WriteLine(new List<int>() { 10, 15, 25, 50 } is [10, _, < 26, 50 or 140]); // discards the second element of the list which is '15' and verifies the conditions only for the others -> true
+
+            //suppose that you want to discard one or more elements from checking the condition, then user .. pattern matching
+            Console.WriteLine(new List<int>() { 10, 15, 30, 345, 25, 50 } is [10, .., < 26, 50 or 100]); // it starts checking from the beginning and from the end the two elements, then it does not check the rest => true
+        
+        
+        
+        }
+    }
+}
